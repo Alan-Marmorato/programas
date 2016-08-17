0c01bf1e25c922b580ad30df126af1561489cd7d
Alan Marmorato <alanmarmoratosud@gmail.com>
2016-08-16 23:37:06 -0300

    Templates video 6 nao esta renderizando direito

---
+++ Template/app/cursos/cursos.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+@Component({
+moduleId: module.id,
+selector: 'cursos-lista',
+
+
+
+
+
+ templateUrl: 'cursos.component.html'
+})
+
+export class CursosComponent {
+
+ nomePortal = 'Alan.trainning';
+
+ cursos = ['Angular 2', 'Java', 'Ext js'];
+
+
+ }
