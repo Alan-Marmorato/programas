9ee940dda2507ad75a3a2ec332a40bf5bd945893
Alan Marmorato <alanmarmoratosud@gmail.com>
2016-08-16 22:16:55 -0300

    Exercicios das video aula 5 loiane de Angular

---
+++ 02-primeiro-componente/app/primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+  selector:'meu-primeiro-componente',
+  template: ` <h2> Meu primeiro componente Angular </h2>
+
+  `
+})
+export class MeuPrimeiroComponent {}
